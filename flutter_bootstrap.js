b6f541b31089c6ca08b7a0a883ee84a96ffdcf29
Thien <thien2357@gmail.com>
2024-11-10 14:15:59 +0700

    commit deploy

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"36335019a8eab588c3c2ea783c618d90505be2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2239827257"
+    serviceWorkerVersion: "3222995667"
   }
 });
